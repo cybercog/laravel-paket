0f8c463c6054c481f8505150f802e31bbf7d9fdf
antonkomarev <ell@cybercog.su>
2019-06-30 00:56:50 +0300

    Add configurable base dashboard URI

---
+++ config/paket.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of Laravel Paket.
+ *
+ * (c) Anton Komarev <anton@komarev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Paket URI
+    |--------------------------------------------------------------------------
+    |
+    | This is the URI path where Paket will be accessible from. Feel free
+    | to change this path to anything you like. Note that the URI will not
+    | affect the paths of its internal API that aren't exposed to users.
+    |
+    */
+
+    'uri' => env('PAKET_URI', 'paket'),
+
+];
