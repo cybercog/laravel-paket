156b00204a7a7bc8065c9ed817ce14c049894a36
Anton Komarev <1849174+antonkomarev@users.noreply.github.com>
2019-06-30 03:59:25 +0300

    Add Authorize middleware (#16)
    Add Authorize middleware

---
+++ src/Http/Middlewares/Authorize.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of Laravel Paket.
+ *
+ * (c) Anton Komarev <anton@komarev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Cog\Laravel\Paket\Http\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+final class Authorize
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return app()->environment('local') ? $next($request) : abort(403);
+    }
+}
